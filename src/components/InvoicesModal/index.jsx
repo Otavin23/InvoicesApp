5f93447f87e8a6c004f7427b86e5dcad714dc270
Otavin23 <alvesotavio132@gmail.com>
2022-03-10 11:48:32 -0300

    Algumas alterações no modal

--- src/components/InvoicesModal/index.jsx
+++ src/components/InvoicesModal/index.jsx
@@ -2,17 +2,68 @@ import React from "react"
 import * as S from "./style"
 
 const InvoicesModal = () => {
-
-    
+  const location = ["City", "Post Code", "Country"]
 
   return (
     <S.Modal>
-      <div className="container">
-        <form>
+      <S.Container>
+        <S.Formulario>
+        <h2>Create Invoice</h2>
+          <div className="containerFormCity">
+            <div className="inputsP">
+              <p className="roxo">Bill From</p>
+              <label>Street Address</label>
+              <input type="text" />
+            </div>
+            <div className="inputsRegion">
+              <div>
+                <label>City</label>
+                <input type="text" />
+              </div>
+              <div>
+                <label>Post Code</label>
+                <input type="text" />
+              </div>
+              <div>
+                <label>Country</label>
+                <input type="text" />
+              </div>
+            </div>
+          </div>
+          <div className="billTo inputsP">
+            <p className='roxo'>Bill To</p>
+            <label>Client's sName</label>
+            <input type="text" />
+            <label>Client's Email</label>
+            <input type="email" placeholder="e.g.email@example.com" />
             <label>Street Address</label>
-            <input type="text" placeholder="Exemplo"
-/>        </form>
-      </div>
+            <input type="text" />
+            <div className="flex-column">
+              {location.map((el,index)=> (
+                <>
+                <div key={index}>
+                  <label>{el}</label>
+                  <input type="text" />
+                </div>
+                </>
+              ))}
+            </div>
+            <label>Description</label>
+            <input type="text" />
+          </div>
+          <div className='send'>
+            <p>Discord</p>
+            <div>
+              <button className="save as Draft">
+                Save as Draft 
+              </button>
+              <button>
+                Save & Send
+              </button>
+            </div>
+          </div>
+        </S.Formulario>
+      </S.Container>
     </S.Modal>
   )
 }
--- src/components/InvoicesModal/style.js
+++ src/components/InvoicesModal/style.js
@@ -2,10 +2,16 @@ import styled from "styled-components"
 
 export const Modal = styled.div`
     position: absolute;
-    background: red;
+    background: #FEFEFF;
     left: 0px;
+    min-width: 20%;
     min-height:100vh;
-    z-index: 100;
+    border-top-right-radius: 1.5rem;
+    border-bottom-right-radius: 1.5rem;
+    //z-index: 2;
+    display: flex; 
+    justify-content: center;
+    align-items: center;
     &::before{
         content: "";
         height:100vh;
@@ -16,6 +22,49 @@ export const Modal = styled.div`
     }   
 `
 export const Container = styled.div`
-
-    
+    max-width: 90%;
+    //min-height: 90vh;
+`
+export const Formulario = styled.form`
+    max-width: 90%;
+    max-height: 50%;
+    .roxo{
+        color: #896CFB;
+        font-size: 12px;
+        font-weight: 700;
+    }
+    label{
+        font-weight: 500;
+        opacity: 0.7;
+        font-size: 12px;
+    }
+    .containerFormCity{
+    }
+    .inputsRegion{
+        max-width: 100%;
+        display: flex;
+        justify-content: center;
+        margin-top: 1rem;
+    }
+    .inputsP{
+        display: flex;
+        flex-direction: column;
+    }
+    .flex-column{
+        display: flex;
+    }
+    .send{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    input{
+        min-height: 50%;
+        min-height: 35px;
+        margin-bottom: 0.5rem;
+        &:focus{
+            outline: none;
+            border: 1px solid #B06DC5;
+        }
+    }
 `
